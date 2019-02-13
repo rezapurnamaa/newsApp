3e9a7d0558bf672dedf814f93b6f51303d9f0fcd
Reza  Purnama Arief <reza.arief@bonial.com>
2019-02-13 22:49:55 +0100

    Initial Commit

---
+++ newsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  newsApp
+//
+//  Created by Reza Arief on 13.02.19.
+//  Copyright © 2019 Reza Arief. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
